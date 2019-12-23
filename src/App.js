dcb71a423142739c6b1ff9fa636a866947c3872f
Felipe De Bene <benfelip@amazon.com>
2019-12-23 21:36:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import GitHub from './GitHub';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavLink from 'react-bootstrap/NavLink';
+
+
+class App extends Component {       
+  render() {        
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+export default App;
+
+class Header extends Component {
+  render(){
+    return (      
+        <BrowserRouter>
+          <div> 
+
+<Navbar bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Oktank</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/github">GitHub</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+                        
+            <Switch>
+              <Route path="/github" component={GitHub} />              
+              <Route exact path="/" component={Home} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>              
+    )
+  }
 }
 
-export default App;
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        Home        
+      </div>
+    )
+  }
+}
+
+class NotFound extends Component {
+  render(){
+    return <div>Not Found</div>
+  }
+}
